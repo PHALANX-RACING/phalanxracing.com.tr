23c59f610acb267cf0aef49590d5d6177a69aaa0
Ebru Tokatlı <info.phalanxracing@gmail.com>
2026-07-10 18:03:27 +0300

    Add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Kendi domainini alınca burayı güncelle (ör. https://phalanxracing.com.tr)
-  site: 'https://phalanx-racing.github.io',
+  site: 'https://phalanxracing.com.tr',
 });
